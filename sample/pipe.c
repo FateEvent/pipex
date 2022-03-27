d358008052898e3b23e4bc61c745b2a6bd8e82ed
FateEvent <fabio.venturini.1989@gmail.com>
2022-03-27 21:32:50 +0200

    sampling

---
+++ sample/pipe.c
@@ -0,0 +1,61 @@
+// C program to demonstrate use of fork() and pipe()
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	int		fd[2];
+	int		arr[] = {1, 2, 3, 11, 4, 5, 7, 9};
+	int		arrSize = sizeof(arr) / sizeof(int);
+	int		start, end;
+	if (pipe(fd) == -1)
+	{
+		fprintf(stderr, "Pipe Failed");
+		return (1);
+	}
+	int		id = fork();
+	if (id == -1)
+	{
+		fprintf(stderr, "fork Failed");
+		return (2);
+	}
+	if (id == 0)
+	{
+		start = 0;
+		end = arrSize / 2;
+	}
+	else
+	{
+		start = arrSize / 2;
+		end = arrSize;
+	}
+	int	sum = 0;
+	int	i = start;
+	while (i < end)
+	{
+		sum += arr[i];
+		i++;
+	}
+	printf("Calculated partial sum: %d\n", sum);
+	if (id == 0)
+	{
+		close(fd[0]);
+		write (fd[1], &sum, sizeof(sum));
+		close(fd[1]);
+	}
+	else
+	{
+		int	sumFromChild;
+		close(fd[1]);
+		read(fd[0], &sumFromChild, sizeof(sumFromChild));
+		close(fd[0]);
+		int	totalSum = sum + sumFromChild;
+		printf("Total sum is %d\n", totalSum);
+		wait(NULL);
+	}
+}
