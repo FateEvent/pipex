2933cbb6046ae75340897f3d55a4e5b3a0b2252f
FateEvent <fabio.venturini.1989@gmail.com>
2022-06-10 18:22:38 +0200

    yay!

--- pipex_step_two/srcs/hd_managing.c
+++ pipex_step_two/srcs/hd_managing.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:53:59 by faventur          #+#    #+#             */
-/*   Updated: 2022/06/10 17:39:26 by faventur         ###   ########.fr       */
+/*   Updated: 2022/06/10 18:22:13 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ static int	ope_and_write(char **arr, char *av[])
 		write(fd, "\n", 1);
 		i++;
 	}
+	close(fd);
+	ft_arr_freer(arr);
 	return (fd);
 }
 
@@ -49,7 +51,9 @@ t_var	hd_managing(int ac, char *av[])
 	}
 	arr = ft_split(temp, '\n');
 	var.fd[0] = ope_and_write(arr, av);
-	ft_arr_freer(arr);
+	var.fd[0] = open("temporary.txt", O_RDONLY);
+	if (var.fd[0] < 0)
+		ft_printerror("pipex", "temporary.txt");
 	var.fd[1] = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (var.fd[1] < 0)
 		ft_printerror("pipex", av[ac - 1]);
