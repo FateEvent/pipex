a9b8ff75a085d7db397ef4351b0b62f0e09b2ea5
FateEvent <fabio.venturini.1989@gmail.com>
2022-07-05 15:45:17 +0200

    .

--- pipex_step_two/includes/pipex.h
+++ pipex_step_two/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 17:17:19 by faventur          #+#    #+#             */
-/*   Updated: 2022/06/23 19:18:50 by faventur         ###   ########.fr       */
+/*   Updated: 2022/07/05 15:45:03 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <sys/stat.h>
-# include <string.h>
 # include <errno.h>
 # include <stdio.h>
 # include <ft_fprintf.h>
